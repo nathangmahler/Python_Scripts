5a537250c6f2f017f065a95fe3b2611f50f7d47b
Nathan Mahler <nathan.mahler87@gmail.com>
2020-08-17 15:39:52 -0600

    Basics

---
+++ py_basics.py
@@ -0,0 +1,18 @@
+name = 'Alice'
+if name == 'Alice':
+    print('Hi Alice')
+print('Done')
+
+spam = 0
+while spam < 5:
+    print('Hello World')
+    spam = spam + 1
+
+while True:
+    print('Hello World')
+    name = ''
+    
+while name != 'your name':
+    print('please print you name')
+    name = input()
+print('Thank you')
